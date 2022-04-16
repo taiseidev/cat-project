5eff2f3adedb3ea6bb96cf52364674e5747f33bf
onishi taisei <taisei.onishi@axia-works.com>
2022-04-16 10:45:09 +0900

    コードカバレッジの関連ファイルを追加

---
+++ test/coverage_test.dart
@@ -0,0 +1,7 @@
+/// *** GENERATED FILE - ANY CHANGES WOULD BE OBSOLETE ON NEXT GENERATION *** ///
+
+/// Helper to test coverage for all project files
+// ignore_for_file: unused_import
+import 'package:cat_project/main.dart';
+
+void main() {}
