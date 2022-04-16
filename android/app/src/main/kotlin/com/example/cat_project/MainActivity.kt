0c09291b5a40a0b060f230c0724bca1bc4dc64bd
onishi taisei <taisei.onishi@axia-works.com>
2022-04-16 23:33:07 +0900

    gitignoreのキャッシュを削除

--- android/app/src/main/kotlin/com/example/cat_project/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.cat_project
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- test/coverage_test.dart
+++
@@ -1,7 +0,0 @@
-/// *** GENERATED FILE - ANY CHANGES WOULD BE OBSOLETE ON NEXT GENERATION *** ///
-
-/// Helper to test coverage for all project files
-// ignore_for_file: unused_import
-import 'package:cat_project/main.dart';
-
-void main() {}
