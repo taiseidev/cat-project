cf999ff7deb0e50b7011f0fbcec0368b77f38e6b
onishi taisei <taisei.onishi@axia-works.com>
2022-04-17 11:36:46 +0900

    GithubActionsテスト

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'cat-app',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
