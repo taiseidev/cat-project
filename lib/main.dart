cc08353f3ba94d62b2e54ade20c227f6548e3afb
onishi taisei <taisei.onishi@axia-works.com>
2022-04-21 23:27:58 +0900

    画面を縦のみ許容

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown],
+  );
   runApp(const MyApp());
 }
 
