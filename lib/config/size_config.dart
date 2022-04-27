18f3de16551c6366c682e71aa193fb4b7cd719b0
onishi taisei <taisei.onishi@axia-works.com>
2022-04-27 23:12:49 +0900

    create size setting file

---
+++ lib/config/size_config.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SizeConfig {
+  // width
+  final width4 = const SizedBox(width: 4);
+  final width8 = const SizedBox(width: 8);
+  final width16 = const SizedBox(width: 16);
+  final width32 = const SizedBox(width: 32);
+  final width64 = const SizedBox(width: 64);
+  final width128 = const SizedBox(width: 128);
+  final width256 = const SizedBox(width: 256);
+
+  // height
+  final height4 = const SizedBox(height: 4);
+  final height8 = const SizedBox(height: 8);
+  final height16 = const SizedBox(height: 16);
+  final height32 = const SizedBox(height: 32);
+  final height64 = const SizedBox(height: 64);
+  final height128 = const SizedBox(height: 128);
+  final height256 = const SizedBox(height: 256);
+}
